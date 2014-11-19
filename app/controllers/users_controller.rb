4ddd6d3e3670d49d2b5a812bd219fa231cbbe07c
HudaSoliman <ba@BHL-APP02-TST-U.(none)>
2014-11-19 15:20:26 +0200

    update a user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,9 @@
 class UsersController < ApplicationController
- 
 
   def show
   	@user=User.find(params[:id])
   	@title = @user.name
   end
-
 
   def new
   	@user = User.new
@@ -21,6 +19,21 @@ class UsersController < ApplicationController
 	  	@title = "Sign up"
 	  	render 'new'
  	  end
-   end
+  end
+
+  def edit 
+    @user = User.find(params[:id])
+    @title ="Edit user"
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      redirect_to @user, :flash => {:success => "Profile updated"}
+    else 
+      @title ="Edit user"
+      render 'edit'
+    end
+  end
 end
  
