1c43480dc741466b6e69f898ba379b0158852108
HudaSoliman <ba@BHL-APP02-TST-U.(none)>
2014-11-19 11:04:52 +0200

    Working user sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,7 +12,8 @@ class SessionsController < ApplicationController
   		@title = "Sign in"
   		render 'new' 				#in case of failed access 
   	else 
-  		# Handle successful signin
+  		sign_in user
+  		redirect_to user
   	end
   end
 
